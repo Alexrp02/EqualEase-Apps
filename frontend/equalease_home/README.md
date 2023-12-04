c4910d088fdcc6a45fcaa39191ad83acd88b7366
Alejandro Ramos Peña <aleeexrp02@gmail.com>
2023-12-04 18:51:35 +0100

    First sprint 3 backend commit

--- frontend/equalease_home/README.md
+++
@@ -1,3 +0,0 @@
-# equalease_home FrontEnd
-
-This is the frontend for our Tasks Application, made in Flutter.
--- frontend/equalease_home/test/pruebagoogle.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http;
-
-void main() {
-  test('Prueba de petición a Google', () async {
-    var response = await http.get(Uri.parse('https://www.google.com'));
-    expect(response.statusCode, 200);
-  });
-}
